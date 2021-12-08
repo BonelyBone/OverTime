a963a66f2dc2973d3981bd59e4c996e8dc6b9578
BonelyBone <48034558+BonelyBone@users.noreply.github.com>
2021-12-08 02:55:22 +0000

    Small Stuff
    Small Stuff

--- Assets/Overtime/UI/Scripts/MainMenu.cs
+++ Assets/Overtime/UI/Scripts/MainMenu.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
-    
+
 {
+    public GameObject LevelSelect;
     //Should Call the LoadGame fuction from Gamemanager and Load the Next Level When called... That if it even works
+    private void Awake()
+    {
+        LevelSelect.gameObject.SetActive(false);
+    }
     public void LoadGame()
     {
         GameManager.instance.LoadGame();
@@ -15,5 +22,16 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("We have quit the game");
+    }
+
+    //Should Load the Level Select Menu
+    public void MenuOpenSelect()
+    {
+        LevelSelect.gameObject.SetActive(true);
+    }
+
+    public void MenuOpenSelectClose()
+    {
+        LevelSelect.gameObject.SetActive(false);
     }
 }
